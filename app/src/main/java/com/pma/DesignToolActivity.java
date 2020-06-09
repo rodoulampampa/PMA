426e93e9ccb01f5171efa53ad164cfb2663c214d
rodoula <rodoulampampa@gmail.com>
2020-06-10 02:00:26 +0300

    Make changes to the layout FeasibilityStudyActivity.

--- app/src/main/java/com/pma/DesignToolActivity.java
+++ app/src/main/java/com/pma/DesignToolActivity.java
@@ -4,14 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
--- app/src/main/java/com/pma/FeasibilityStudyActivity.java
+++ app/src/main/java/com/pma/FeasibilityStudyActivity.java
@@ -2,13 +2,52 @@ package com.pma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class FeasibilityStudyActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editq1ans;
+    EditText editq2ans;
+    EditText editq3ans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feasibility_study);
+
+        button = (Button)findViewById(R.id.Save);
+        editq1ans = (EditText)findViewById(R.id.q1ans);
+        editq2ans = (EditText)findViewById(R.id.q2ans);
+        editq3ans = (EditText)findViewById(R.id.q3ans);
+
+        button.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.i("EditText Value = ", editq1ans.getText().toString());
+                        Log.i("EditText Value = ", editq2ans.getText().toString());
+                        Log.i("EditText Value = ", editq3ans.getText().toString());
+
+                        // setup the alert builder
+                        AlertDialog.Builder builder = new AlertDialog.Builder(FeasibilityStudyActivity.this);
+                        builder.setTitle("Result");
+
+                        if (Float.parseFloat(editq1ans.getText().toString()) >= (15000) && Float.parseFloat(editq2ans.getText().toString()) >= (30) && Float.parseFloat(editq3ans.getText().toString()) >= (30))
+                        {
+                            // create and show the alert dialog
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        }
+                    }
+                });
     }
 }
+
